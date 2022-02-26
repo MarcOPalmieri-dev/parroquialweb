dd7533a540c6631042d63c2713a7029fb6e3cd49
Marco Palmieri <marquitopalmieri@gmail.com>
2022-02-25 23:04:42 -0300

    completada la mitad de la seccion normativas

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 
-AOS.init();
+// AOS.init();
 
 const menu_button = document.getElementsByClassName("menu")[0];
 const off_canvas_menu = document.querySelector("#menu");
@@ -21,3 +21,46 @@ off_canvas_menu.addEventListener('animationend',()=>{
     off_canvas_menu.classList.remove("menu__animation")
 })
 
+
+// Read more function
+
+const [...readmore_button] = document.getElementsByClassName('more');
+
+// readMoreFunction = (e)=>{
+//     if(e.target.dataset.number == "1"){
+//         const p_element = e.path[0].previousElementSibling
+//         if(p_element.classList.contains("hidden")){
+//             p_element.classList.remove("hidden")
+//             console.log(this)
+//         }
+//     }
+// }
+readmore_button.forEach(button=>{
+    
+    button.addEventListener('click',(e)=>{
+        const p_element = e.path[0].previousElementSibling;
+        
+        if(p_element.classList.contains("hidden")){
+            p_element.classList.remove("hidden");
+            button.innerText="Leer menos...";
+
+        }else if(!p_element.classList.contains("hidden")){
+            p_element.classList.add("hidden")
+            button.innerText="Leer más..."
+        }
+    })
+})
+
+// button.addEventListener('click',(e)=>{
+//     if(e.target.dataset.number == "1"){
+//         const p_element = e.path[0].previousElementSibling;
+        
+//         if(p_element.classList.contains("hidden")){
+//             p_element.classList.remove("hidden");
+//             button.innerText="Leer menos...";
+
+//         }else if(!p_element.classList.contains("hidden")){
+//             p_element.classList.add("hidden")
+//             button.innerText="Leer más..."
+//         }
+//     })
